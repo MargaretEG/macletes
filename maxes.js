897eec98c3f5e9273c0911d808b0236276dc51e8
margaretgiesel <mgiesel@macalester.edu>
2014-11-12 09:51:55 -0600

    Completed PDF and Maxespages

--- maxes.js
+++ maxes.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-  $("button").click(function(){
-    $.ajax({url:"testing.html",success:function(result){
-      $(".Absolute-Center").html(result);
-    }});
-  });
-});
+// $(document).ready(function(){
+//   $("button").click(function(){
+//     $.ajax({url:"testing.html",success:function(result){
+//       $(".Absolute-Center").html(result);
+//     }});
+//   });
+// });
 
 // <!DOCTYPE html>
 // <html>
@@ -19,3 +19,45 @@ $(document).ready(function(){
 
 // </body>
 // </html>
+// Class to represent a row in the seat reservations grid
+// Class to represent a row in the seat reservations grid
+
+// Class to represent a row in the seat reservations grid
+function SeatReservation(lift, max) {
+    var self = this;
+    self.lift = lift;
+    self.max = max;
+}
+
+// Overall viewmodel for this screen, along with initial state
+function ReservationsViewModel() {
+    var self = this;
+
+    
+    // Editable data
+    self.maxes = ko.observableArray([
+        new SeatReservation("Steve", "dfa"),
+        new SeatReservation("Bert", "self.availableMeals[0]")
+    ]);
+
+    
+    // Operations
+    self.addMax = function() {
+        self.maxes.push(new SeatReservation("", ""));
+    }
+}
+
+ko.applyBindings(new ReservationsViewModel());
+
+
+ $(document).ready(function(){
+
+    $("#edit").click(function(){
+        $(".information").hide();
+        $(".informationEdit").show();
+
+    $("#done").click(function(){
+            $(".information").hide();
+		$(".informationEdit").show();
+});
+	});	});
