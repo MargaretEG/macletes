3746ae920f46b1feb43b0c3d960639e53caf53b1
MargaretEG <mgiesel@macalester.edu>
2014-11-13 15:30:58 -0600

    Delete maxes.js

--- maxes.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $("button").click(function(){
-    $.ajax({url:"testing.html",success:function(result){
-      $(".Absolute-Center").html(result);
-    }});
-  });
-});
-
-// $( ".Absolute-Center" ).load( "testing.html #knocked" );
